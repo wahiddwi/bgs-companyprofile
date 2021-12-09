50de1263b82d3ffb7fb9900eeff230bd04e09e9b
wahiddwi <wahiddwi7@gmail.com>
2021-12-09 13:06:40 +0700

    icon & layouting

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -7,9 +7,9 @@
         <a href="{{route('admin.layanan.index')}}"></a>
       </div>
       <ul class="sidebar-menu">
-          <li><a class="nav-link" href="{{route('admin.layanan.index')}}"><i class="fas fa-pencil-ruler"></i> <span>Layanan</span></a></li>
-          <li><a class="nav-link" href="{{ route('admin.lokasi.index') }}"><i class="fas fa-pencil-ruler"></i> <span>Lokasi</span></a></li>
-          <li><a class="nav-link" href="{{ route('admin.kontak.index') }}"><i class="fas fa-pencil-ruler"></i> <span>Kontak</span></a></li>
+          <li><a class="nav-link" href="{{route('admin.layanan.index')}}"><i class="fas fa-cogs"></i> <span>Layanan</span></a></li>
+          <li><a class="nav-link" href="{{ route('admin.lokasi.index') }}"><i class="fas fa-thumbtack"></i> <span>Lokasi</span></a></li>
+          <li><a class="nav-link" href="{{ route('admin.kontak.index') }}"><i class="fas fa-envelope-open-text"></i> <span>Kontak</span></a></li>
         </ul>
     </aside>
   </div>
